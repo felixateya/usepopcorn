a247262da39669e1c646b20b563ee2e43888a157
felixateya <mulaafelix6@gmail.com>
2024-04-11 11:35:28 +0300

    changed http to https for hosting

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -48,7 +48,7 @@ console.log(isTop)
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
